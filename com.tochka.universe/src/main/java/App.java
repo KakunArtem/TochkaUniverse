72459c76cbd211d661e796c44ff2637d81ce1f49
KakunArtem <thecreep09@gmail.com>
2021-01-22 23:20:15 +0200

    initial commit

---
+++ com.tochka.universe/src/main/java/App.java
@@ -0,0 +1,12 @@
+import static UI.MainWindowHandler.getMainWindow;
+
+public class App {
+    public static void main(String[] args) {
+        getMainWindow();
+
+    }
+
+
+
+
+}
---
+++ com.tochka.universe/src/main/java/UI/MainWindowHandler.java
@@ -0,0 +1,33 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindowHandler {
+    private static MainWindowHandler instance;
+    private final JFrame mainWindow = new JFrame("Tochka Universe");
+    private final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public static MainWindowHandler getMainWindow() {
+        if (instance == null) {
+            instance = new MainWindowHandler();
+        }
+        return instance;
+    }
+
+    private MainWindowHandler() {
+        this.mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.mainWindow.setVisible(true);
+        //size to props
+        this.mainWindow.setSize(804, 804);
+        this.centerMainWindow();
+    }
+
+    private void centerMainWindow() {
+        this.mainWindow.setLocation(
+                this.dim.width / 2 - this.mainWindow.getSize().width / 2,
+                this.dim.height / 2 - this.mainWindow.getSize().height / 2);
+    }
+
+
+}
